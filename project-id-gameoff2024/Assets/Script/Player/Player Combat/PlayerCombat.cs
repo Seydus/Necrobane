b6e92c23365c8ee1f2609a0127c2444575ebbef3
Seydus <weljohncatanpatan@gmail.com>
2024-11-24 23:34:05 +0800

    added sword defend method for animation

--- project-id-gameoff2024/Assets/Script/Player/Player Combat/PlayerCombat.cs
+++ project-id-gameoff2024/Assets/Script/Player/Player Combat/PlayerCombat.cs
@@ -128,22 +128,61 @@ public class PlayerCombat : MonoBehaviour
             WeaponHolder.weapon.PlayerCombat = this;
         }
 
-        WeaponHolder.weapon.HandleBasicAttack();
-        WeaponHolder.weapon.HandleSuperAttack();
+        WeaponHolder.weapon.SetAnimationLayer();
+
+        //Weapon currentWeapon = null;
+
+        //switch(WeaponHolder.weapon.weaponSO.WeaponType)
+        //{
+        //    case WeaponSO.Weapons.PowerGlove:
+        //        currentWeapon = (Gloves)WeaponHolder.weapon;
+        //        break;
+        //    case WeaponSO.Weapons.Sword:
+        //        currentWeapon = (Sword)WeaponHolder.weapon;
+        //        break;
+        //    default:
+        //        break;
+        //}
+
+
+        //if (currentWeapon == null)
+        //{
+        //    Debug.LogError("Current weapon can't be found");
+        //    return;
+        //}
+
+        WeaponHolder.weapon.HandleFirstAttack();
+        WeaponHolder.weapon.HandleSecondaryAttack();
     }
 
     private void PerformBasicAttack()
     {
-        WeaponHolder.weapon.PerformBasicAttack();
+        if(WeaponHolder == null)
+        {
+            return;
+        }
+
+        WeaponHolder.weapon.PerformFirstAttack();
     }
 
     private void PerformSuperAttack()
     {
-        WeaponHolder.weapon.PerformSuperAttack();
+        if (WeaponHolder == null)
+        {
+            return;
+        }
+
+        WeaponHolder.weapon.PerformSecondaryAttack();
     }
 
     private void FinishAttack()
     {
+        if (WeaponHolder == null)
+        {
+            IsAttacking = false;
+            return;
+        }
+
         WeaponHolder.weapon.FinishAttack();
     }
 
