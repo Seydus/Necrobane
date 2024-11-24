fdba56a42853a0a917e7101447e43d157697fc37
GerpisPerevorker <147097764+GerpisPerevorker@users.noreply.github.com>
2024-11-24 23:14:52 +0200

    Revert "Merge pull request #67 from Seydus/sai3"
    This reverts commit 0fcfd3046e35a9216329d72f715e299b68df6d4d, reversing
    changes made to 170d8c2392b3e2b9b066a334f7aa80dc8c24c7b5.

--- project-id-gameoff2024/Assets/Script/Weapon/Weapon.cs
+++ project-id-gameoff2024/Assets/Script/Weapon/Weapon.cs
@@ -5,11 +5,10 @@ public abstract class Weapon : MonoBehaviour
     public WeaponSO weaponSO;
 
     public PlayerCombat PlayerCombat { get; set; }
-    public abstract void SetAnimationLayer();
     public abstract void HandleAttack(Enemy enemy, float damage);
-    public abstract void HandleFirstAttack();
-    public abstract void HandleSecondaryAttack();
-    public abstract void PerformFirstAttack();
-    public abstract void PerformSecondaryAttack();
+    public abstract void HandleBasicAttack();
+    public abstract void HandleSuperAttack();
+    public abstract void PerformBasicAttack();
+    public abstract void PerformSuperAttack();
     public abstract void FinishAttack();
 }
--- project-id-gameoff2024/Assets/Script/Weapon/WeaponType/Sword.cs
+++ project-id-gameoff2024/Assets/Script/Weapon/WeaponType/Sword.cs
@@ -2,45 +2,37 @@ using UnityEngine;
 
 public class Sword : Weapon
 {
-    public override void HandleFirstAttack()
+    public override void HandleBasicAttack()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Attacking with sword.");
-            //PlayerCombat.IsAttacking = true;
+            PlayerCombat.IsAttacking = true;
 
-            //PlayerCombat.PlayerController.maxSpeed /= 2f;
-            //PlayerCombat.PlayerAnimation.PeformBasicSwordAttackAnim();
+            PlayerCombat.PlayerController.maxSpeed /= 2f;
+            PlayerCombat.PlayerAnimation.PeformBasicSwordAttackAnim();
         }
     }
 
-    public override void HandleSecondaryAttack()
+    public override void HandleSuperAttack()
     {
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButtonDown(1))
         {
             if (PlayerCombat.PlayerProfile.playerStamina >= weaponSO.WeaponStaminaCost)
             {
-                PlayerCombat.PlayerProfile.isDefending = true;
-                //PlayerCombat.IsAttacking = true;
+                PlayerCombat.IsAttacking = true;
 
-                //PlayerCombat.PlayerController.maxSpeed /= 2;
+                PlayerCombat.PlayerController.maxSpeed /= 2;
                 PlayerCombat.PlayerProfile.DeductStamina(weaponSO.WeaponStaminaCost);
-                // PlayerCombat.PlayerAnimation.PerformDefendSwordAttackAnim();
-                Debug.Log("Defending with Sword");
+                PlayerCombat.PlayerAnimation.PerformDefendSwordAnim();
             }
             else
             {
-                PlayerCombat.PlayerProfile.isDefending = false;
                 Debug.Log("You don't have enough mana.");
             }
         }
-        else
-        {
-            PlayerCombat.PlayerProfile.isDefending = false;
-        }
     }
 
-    public override void PerformFirstAttack()
+    public override void PerformBasicAttack()
     {
         if (PlayerCombat.WeaponCheckCastInfo())
         {
@@ -65,10 +57,29 @@ public class Sword : Weapon
         PlayerCombat.PlayerController.maxSpeed = PlayerCombat.oldMaxSpeed;
     }
 
-    public override void PerformSecondaryAttack()
+    public override void PerformSuperAttack()
     {
+        if (PlayerCombat.WeaponCheckCastInfo())
+        {
+            if (PlayerCombat.sphereCastHit.transform.TryGetComponent<Enemy>(out Enemy enemy))
+            {
+                PlayerCombat.StartCoroutine(PlayerCombat.PlayerCombatCamera.CameraShake(new CameraCombatInfo(0.20f, 0.020f, Vector3.zero)));
+                PlayerCombat.InitHitVFX(PlayerCombat.sphereCastHit.point);
 
-        Debug.Log("Secondary");
+                HandleAttack(enemy, weaponSO.WeaponSuperAttackDamage);
+
+                AkSoundEngine.PostEvent("Play_HitBones", PlayerCombat.gameObject);
+            }
+        }
+        else
+        {
+            if (PlayerCombat.WeaponCheckCastInfo())
+            {
+                PlayerCombat.InitHitVFX(PlayerCombat.sphereCastHit.point);
+            }
+        }
+
+        PlayerCombat.PlayerController.maxSpeed = PlayerCombat.oldMaxSpeed;
     }
 
     public override void HandleAttack(Enemy enemy, float damage)
@@ -81,11 +92,5 @@ public class Sword : Weapon
     public override void FinishAttack()
     {
         PlayerCombat.IsAttacking = false;
-    }
-
-    public override void SetAnimationLayer()
-    {
-        PlayerCombat.PlayerAnimation.anim.SetLayerWeight(1, 0f);
-        PlayerCombat.PlayerAnimation.anim.SetLayerWeight(2, 1f);
     }
 }
