8e6d9533f0409e182687ce0cd6678254ce5db1d0
Zenunim666 <hillemaximilian@gmail.com>
2024-11-29 00:56:25 +0100

    +

---
+++ project-id-gameoff2024/Assets/Script/Weapon/WeaponData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class WeaponData 
+{
+    public WeaponSO.Weapons WeaponType { get; set; }
+    public string WeaponName { get; set; }
+    public float WeaponBasicDamage { get; set; }
+    public float WeaponStaminaCost { get; set; }
+    public float WeaponSuperAttackDamage { get; set; }
+    public float WeaponStunDuration { get; set; }
+    public float WeaponBlockDuration { get; set; }
+}
