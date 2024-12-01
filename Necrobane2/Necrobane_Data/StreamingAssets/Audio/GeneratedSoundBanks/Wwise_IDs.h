26cbb86b3b9a25b6d1b8582563e14a0be2855525
Zenunim666 <hillemaximilian@gmail.com>
2024-12-01 22:43:18 +0100

    pause menu fix

---
+++ Necrobane2/Necrobane_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,137 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMBI = 2616876087U;
+        static const AkUniqueID PLAY_BONESTEPS = 1512643089U;
+        static const AkUniqueID PLAY_BOSS = 1693071901U;
+        static const AkUniqueID PLAY_BUTTONSFX = 1875815139U;
+        static const AkUniqueID PLAY_CHOPS = 3488755065U;
+        static const AkUniqueID PLAY_DROP_ITEM = 1363530945U;
+        static const AkUniqueID PLAY_EQUIP_BLADE = 1527095013U;
+        static const AkUniqueID PLAY_EQUIP_FIST = 1554763031U;
+        static const AkUniqueID PLAY_FIREBOLT = 79128399U;
+        static const AkUniqueID PLAY_FIREBOLT_EXPLOSION = 38142515U;
+        static const AkUniqueID PLAY_FIREWALL = 4110807830U;
+        static const AkUniqueID PLAY_FISTSWING = 1452450734U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_HITBONES = 372480478U;
+        static const AkUniqueID PLAY_JUMP = 3689126666U;
+        static const AkUniqueID PLAY_KEY_PICKUP = 2881789206U;
+        static const AkUniqueID PLAY_LEVER = 3196500978U;
+        static const AkUniqueID PLAY_MENU = 1278378707U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_OPEN_GATE = 1127904212U;
+        static const AkUniqueID PLAY_PARRY = 4115969904U;
+        static const AkUniqueID PLAY_PERFECTPARRYSTINGER = 2908761997U;
+        static const AkUniqueID PLAY_SKELETONDEATH = 2507006415U;
+        static const AkUniqueID PLAY_SW = 2078302632U;
+        static const AkUniqueID PLAY_SWINGS2H = 1187857689U;
+        static const AkUniqueID PLAY_UNLOCK_GATE = 356987792U;
+        static const AkUniqueID STOP_AMBI = 1937570009U;
+        static const AkUniqueID STOP_BOSS = 3511702883U;
+        static const AkUniqueID STOP_FIREBOLT = 2555710829U;
+        static const AkUniqueID STOP_MENU = 2914981333U;
+        static const AkUniqueID STOP_MUSIC = 2837384057U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYER
+        {
+            static const AkUniqueID GROUP = 1069431850U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID BOSS = 1560169506U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace PLAYER
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRAVEL = 2185786256U;
+                static const AkUniqueID STONE = 1216965916U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BOSSFIGHT = 580146960U;
+                static const AkUniqueID COMBAT = 2764240573U;
+                static const AkUniqueID EXPLORATION = 2582085496U;
+            } // namespace SWITCH
+        } // namespace MUSIC
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BGM = 412724365U;
+        static const AkUniqueID MASTER = 4056684167U;
+        static const AkUniqueID SECRETWALL = 876875105U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIANCE = 2981377429U;
+        static const AkUniqueID COMBAT = 2764240573U;
+        static const AkUniqueID ENEMY = 2299321487U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID INTERACTABLES = 181270742U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MECHANISMS = 4015094827U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID PLAYERSFX = 4283257371U;
+        static const AkUniqueID REVERBS = 3545700988U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID BIGROOM = 1495464960U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
